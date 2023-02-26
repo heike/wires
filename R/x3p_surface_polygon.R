75fb245ad92ad3dcf8591184ff3535bba4c8b163
Yuhang (Tom) LIN <yhlin@iastate.edu>
2023-02-26 06:45:27 -0600

    Remove mask black border

--- R/x3p_surface_polygon.R
+++ R/x3p_surface_polygon.R
@@ -23,27 +23,28 @@
 #'   x3p <- x3p %>% x3p_surface_polygon(sample=1)
 #'   x3p_image(x3p, size = dim(x3p$surface.matrix), zoom=.6)
 #' }
-x3p_surface_polygon <- function(x3p, colour = "red", sample=10,center = NULL, concavity = 1.5) {
+x3p_surface_polygon <- function(x3p, colour = "red", sample = 10, center = NULL, concavity = 1.5) {
   stopifnot("x3p" %in% class(x3p), is.numeric(concavity), concavity > 0)
 
   boundary <- x3p %>% x3p_boundary_points(sample = sample)
   polygon_inside <- inside_polygon(boundary$x, boundary$y,
-                                   concavity = concavity,
-                                   center = center)
+    concavity = concavity,
+    center = center
+  )
   dims <- dim(x3p$surface.matrix)
   resolution <- x3p %>% x3p_get_scale()
 
   mask_png <- tempfile(fileext = ".png")
 
-  CairoPNG(mask_png,width = dims[1], height=dims[2], units="px")
-  par(mar=c(0,0,0,0))
-  plot.default(x=c(1, dims[1])*resolution, y=c(1, dims[2])*resolution, type="n", frame.plot=FALSE, axes = FALSE)
-  polygon(rbind(polygon_inside[,c("x", "y")],polygon_inside[1,c("x", "y")]), col=colour)
-#savePlot("mask.png",device = dev.cur())
+  CairoPNG(mask_png, width = dims[1], height = dims[2], units = "px")
+  par(mar = c(0, 0, 0, 0))
+  plot.default(x = c(1, dims[1]) * resolution, y = c(1, dims[2]) * resolution, type = "n", frame.plot = FALSE, axes = FALSE)
+  polygon(rbind(polygon_inside[, c("x", "y")], polygon_inside[1, c("x", "y")]), col = colour, border = FALSE)
+  # savePlot("mask.png",device = dev.cur())
   dev.off()
   mask <- png::readPNG(mask_png)
 
   # this replaces any existing mask
 
-  x3p_add_mask(x3p, mask=as.raster(mask))
+  x3p_add_mask(x3p, mask = as.raster(mask))
 }
